7a1103ad0dbd775f4c079c908439ecd6c84db931
Sergey <ilyasovsa93@gmail.com>
2020-09-18 17:45:40 +0300

    Init commit

---
+++ JSONparser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JSONparser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
